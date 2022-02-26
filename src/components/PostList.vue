28f74266ca4555b5bec2bdecdbe538a570e64bd7
hemiahwu <309595700@qq.com>
2022-02-26 18:03:40 +0800

    13props传值

---
+++ src/components/PostList.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  posts: Array,
+});
+</script>
+
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,16 @@
 <script setup>
+import PostList from "../components/PostList.vue";
 import { ref, reactive, computed, watch, watchEffect } from "vue";
 
-const names = ref([
-  { name: "小猫" },
-  { name: "小猫猫" },
-  { name: "小狗" },
-  { name: "小猪" },
-  { name: "小爱" },
+const posts = ref([
+  { title: "跟米斯特吴学习全新的Vue.js", body: "vue", id: 1 },
+  { title: "跟米斯特吴学习全新的React.js", body: "react", id: 2 },
 ]);
-
-const search = ref("");
-
-const matchNames = computed(() => {
-  return names.value.filter((item) => item.name.includes(search.value));
-});
-
-// watch([search, names], ([newSearch, newNames], [prevSearch, prevNames]) => {
-//   console.log("watch函数触发了", newSearch, prevSearch);
-// });
-
-// watchEffect(() => {
-//   console.log("watchEffect执行了", search.value);
-// });
-
-const stopWatch = watch(
-  [search, names],
-  ([newSearch, newNames], [prevSearch, prevNames]) => {
-    console.log("watch函数触发了", newSearch, prevSearch);
-  }
-);
-
-const stopEffect = watchEffect(() => {
-  console.log("watchEffect执行了", search.value);
-});
-
-const handleClick = () => {
-  stopWatch();
-  stopEffect();
-};
 </script>
 
 <template>
   <div class="home">
-    <input type="text" v-model="search" />
-
-    <button @click="handleClick">停止监听</button>
-
-    <p v-for="(item, index) in matchNames" :key="index">{{ item.name }}</p>
+    <PostList :posts="posts" />
   </div>
 </template>
 
