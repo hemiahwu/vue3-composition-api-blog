967445cdc71dac6a27b3591b862855b9252ae5d8
hemiahwu <309595700@qq.com>
2022-02-26 17:00:53 +0800

    08computed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,14 @@
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, computed } from "vue";
 
-const refData = ref("mr.wu");
-const reactiveData = reactive("米斯特吴");
-
-const updateRefData = () => {
-  refData.value = "mister.wu";
-};
-
-const updateReactiveData = () => {
-  reactiveData = "米斯特吴123";
-};
+const name = computed(() => {
+  return "米斯特吴";
+});
 </script>
 
 <template>
   <div class="home">
-    <p>RefData: {{ refData }}</p>
-    <button @click="updateRefData">更新RedData</button>
-
-    <p>ReactiveData: {{ reactiveData }}</p>
-    <button @click="updateReactiveData">更新ReactiveData</button>
+    <h1>{{ name }}</h1>
   </div>
 </template>
 
