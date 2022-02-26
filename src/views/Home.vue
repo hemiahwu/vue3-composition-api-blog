ad76df848383390c75a49cf35222e0aae2630083
hemiahwu <309595700@qq.com>
2022-02-26 17:49:41 +0800

    12watch和watchEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, computed } from "vue";
+import { ref, reactive, computed, watch, watchEffect } from "vue";
 
 const names = ref([
   { name: "小猫" },
@@ -14,11 +14,37 @@ const search = ref("");
 const matchNames = computed(() => {
   return names.value.filter((item) => item.name.includes(search.value));
 });
+
+// watch([search, names], ([newSearch, newNames], [prevSearch, prevNames]) => {
+//   console.log("watch函数触发了", newSearch, prevSearch);
+// });
+
+// watchEffect(() => {
+//   console.log("watchEffect执行了", search.value);
+// });
+
+const stopWatch = watch(
+  [search, names],
+  ([newSearch, newNames], [prevSearch, prevNames]) => {
+    console.log("watch函数触发了", newSearch, prevSearch);
+  }
+);
+
+const stopEffect = watchEffect(() => {
+  console.log("watchEffect执行了", search.value);
+});
+
+const handleClick = () => {
+  stopWatch();
+  stopEffect();
+};
 </script>
 
 <template>
   <div class="home">
     <input type="text" v-model="search" />
+
+    <button @click="handleClick">停止监听</button>
 
     <p v-for="(item, index) in matchNames" :key="index">{{ item.name }}</p>
   </div>
