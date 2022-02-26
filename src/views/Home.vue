17b97e69506aea204e5878730cfbd7ac7a893025
hemiahwu <309595700@qq.com>
2022-02-26 17:25:33 +0800

    11搜索

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,20 +3,24 @@ import { ref, reactive, computed } from "vue";
 
 const names = ref([
   { name: "小猫" },
+  { name: "小猫猫" },
   { name: "小狗" },
   { name: "小猪" },
   { name: "小爱" },
 ]);
 
-const search = ref("米斯特吴");
+const search = ref("");
+
+const matchNames = computed(() => {
+  return names.value.filter((item) => item.name.includes(search.value));
+});
 </script>
 
 <template>
   <div class="home">
     <input type="text" v-model="search" />
-    <!--input textarea select -->
-    <p>{{ search }}</p>
-    <p v-for="(item, index) in names" :key="index">{{ item.name }}</p>
+
+    <p v-for="(item, index) in matchNames" :key="index">{{ item.name }}</p>
   </div>
 </template>
 
