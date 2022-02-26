f3657071ce1f5c4008b23ed88186aa732479765d
hemiahwu <309595700@qq.com>
2022-02-26 16:57:30 +0800

    07reactive和ref的区别

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,24 @@
 <script setup>
 import { ref, reactive } from "vue";
 
-const refData = ref({ name: "mr.wu", age: 30 });
-const reactiveData = reactive({ name: "米斯特吴", age: 28 });
+const refData = ref("mr.wu");
+const reactiveData = reactive("米斯特吴");
 
 const updateRefData = () => {
-  refData.value.name = "mister.wu";
+  refData.value = "mister.wu";
 };
 
 const updateReactiveData = () => {
-  reactiveData.name = "米斯特吴123";
+  reactiveData = "米斯特吴123";
 };
 </script>
 
 <template>
   <div class="home">
-    <p>RefData: {{ refData.name }} - {{ refData.age }}</p>
+    <p>RefData: {{ refData }}</p>
     <button @click="updateRefData">更新RedData</button>
 
-    <p>ReactiveData: {{ reactiveData.name }} - {{ reactiveData.age }}</p>
+    <p>ReactiveData: {{ reactiveData }}</p>
     <button @click="updateReactiveData">更新ReactiveData</button>
   </div>
 </template>
