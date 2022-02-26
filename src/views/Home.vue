24bcd25b237e2ebd49466cc4cfffe61dedb0853a
hemiahwu <309595700@qq.com>
2022-02-26 16:25:14 +0800

    04定义属性和方法

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,18 @@
 <script setup>
+let name = "mr.wu";
+let age = 30;
+
+const handleClick = () => {
+  name = "米斯特吴";
+  console.log(name);
+};
 </script>
 
 <template>
-  <div>Home.vue Hello World</div>
+  <div>
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="handleClick">click me</button>
+  </div>
 </template>
 
 <style>
