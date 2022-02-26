b56bc679aed2c10e5193244aa429eea4c22648ac
hemiahwu <309595700@qq.com>
2022-02-26 17:16:23 +0800

    10v-model

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,10 +7,15 @@ const names = ref([
   { name: "小猪" },
   { name: "小爱" },
 ]);
+
+const search = ref("米斯特吴");
 </script>
 
 <template>
   <div class="home">
+    <input type="text" v-model="search" />
+    <!--input textarea select -->
+    <p>{{ search }}</p>
     <p v-for="(item, index) in names" :key="index">{{ item.name }}</p>
   </div>
 </template>
