07788e0f02398cce3d66beb6b6869534175a647a
hemiahwu <309595700@qq.com>
2022-03-01 11:58:38 +0800

    17axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,30 @@
 <script setup>
 import PostList from "../components/PostList.vue";
 import { ref, reactive, computed, watch, watchEffect } from "vue";
+import axios from 'axios'
 
-const posts = ref([
-  { title: "跟米斯特吴学习全新的Vue.js", body: "vue", id: 1 },
-  {
-    title: "跟米斯特吴学习全新的React.js",
-    body: "react Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, omnis reprehenderit. Explicabo incidunt maxime repellendus voluptatum earum ipsum veritatis excepturi, numquam omnis totam soluta quo cupiditate? Praesentium rerum quaerat quod obcaecati est, consectetur ut? Quis, libero similique temporibus reprehenderit sed dolor repellat vel amet rem nisi eius? Ratione dolores ipsa, temporibus autem voluptate aperiam in delectus laboriosam nostrum dolore vitae quo nihil corporis sunt atque soluta similique iure error labore porro dignissimos consequuntur qui ea. Recusandae expedita dolor odio ipsa illo culpa harum quas, rerum asperiores, vel, ullam est omnis quod sit. Vero ipsum natus placeat modi quaerat ullam earum eius! Explicabo, sunt! Aperiam totam alias, facere, expedita commodi maiores deserunt ipsa nihil debitis veniam, similique sequi soluta pariatur laudantium perferendis obcaecati nobis excepturi vitae molestias. Pariatur consequatur perspiciatis vitae ut nemo voluptates voluptas, consectetur adipisci distinctio id voluptate similique numquam eligendi, laborum modi quia facere quae accusantium velit suscipit. Ipsum debitis ea accusamus, ratione odit molestias eius, praesentium, reiciendis temporibus quasi eligendi neque iste quos voluptatum fugit magnam exercitationem ipsam cumque quo esse incidunt! Accusamus nam fugit vel rerum quisquam fugiat fuga quaerat dolor, quidem quae autem ipsum culpa dolore illum. Magnam dolores excepturi voluptates quasi perferendis voluptatibus iste!",
-    id: 2,
-  },
-]);
+const posts = ref([]);
+
+const load = async () => {
+  try {
+    let {data} = await axios("http://localhost:3003/posts")
+    posts.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+load()
 
 const showPosts = ref(true);
 </script>
 
 <template>
   <div class="home">
-    <PostList :posts="posts" v-if="showPosts" />
-    <button @click="showPosts = !showPosts">显示/隐藏 PostList组件</button>
-
-    <button @click="posts.pop()">删除一个博客信息</button>
+    <div v-if="posts.length">
+      <PostList :posts="posts" v-if="showPosts" />
+    </div>
+    <div v-else>加载中....</div>
   </div>
 </template>
 
